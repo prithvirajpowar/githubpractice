a1b7683a00754102b0e347eb26eb1afa0c69f150
Prithviraj Powar <79301072+prithvirajpowar@users.noreply.github.com>
2023-02-21 14:06:54 +0530

    updated java file

--- Sample.java
+++ Sample.java
@@ -1,7 +1,6 @@
 class Sample{
         public static void main(String[] args){
                 System.out.println("Hello World");
-                System.out.println("I am Prithviraj");
         }
 }
 ~
